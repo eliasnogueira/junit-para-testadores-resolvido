55d5b84ff950e7c2605e4b6747fc224a566ac6ec
eliasnogueira <eliasnogueira@192.168.0.3>
2017-12-10 18:06:54 -0200

    Adicionado exercicio7

---
+++ src/test/java/exercicio7/ValidarIdadePorVotacao.java
@@ -0,0 +1,46 @@
+package exercicio7;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import votacao.Votacao;
+
+public class ValidarIdadePorVotacao {
+	
+	@Test
+	public void idadeIgual15Anos_NaoPodeVotar() {
+		assertEquals("Maria, você não pode votar", Votacao.podeVotar("Maria", 2002));
+	}
+	
+	@Test
+	public void idadeIgual16Anos_VotoFacultativo() {
+		assertEquals("Rodrigo, seu voto é facultativo", Votacao.podeVotar("Rodrigo", 2001));
+	}
+	
+	@Test
+	public void idadeIgual17Anos_VotoFacultativo() {
+		assertEquals("João, seu voto é facultativo", Votacao.podeVotar("João", 2000));
+	}
+	
+	@Test
+	public void idadeIgual18Anos_VotoObrigatorio() {
+		assertEquals("Carla, seu voto é obrigatório", Votacao.podeVotar("Carla", 1999));
+	}
+	
+	@Test
+	public void idadeIgual25Anos_VotoObrigatorio() {
+		assertEquals("José, seu voto é obrigatório", Votacao.podeVotar("José", 1992));
+	}
+	
+	@Test
+	public void idadeIgual70Anos_VotoObrigatorio() {
+		assertEquals("Ana, seu voto é obrigatório", Votacao.podeVotar("Ana", 1947));
+	}
+	
+	@Test
+	public void idadeIgual71Anos_VotoFacultativo() {
+		assertEquals("Pedro, seu voto é facultativo", Votacao.podeVotar("Pedro", 1946));
+	}
+
+}
