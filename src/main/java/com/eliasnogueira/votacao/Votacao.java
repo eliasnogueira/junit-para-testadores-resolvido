886ae58fa394b19c267884f14d17aae9474d0aeb
eliasnogueira <eliasnogueira@192.168.0.3>
2017-12-10 18:04:25 -0200

    Classe de suporte aos exercicios de Data Driven

---
+++ src/main/java/com/eliasnogueira/votacao/Votacao.java
@@ -0,0 +1,35 @@
+package com.eliasnogueira.votacao;
+
+import java.util.Calendar;
+
+public class Votacao {
+
+	/*
+	 * Retorna um texto informando se a pessoa pode ou não votar
+	 * dado um nome e a data de nascimento
+	 */
+	public static String podeVotar(String nome, int anoDeNascimento) {
+		String retorno = null;
+		int idade = retornaAnoAtual() - anoDeNascimento;
+		
+		if (idade < 16) {
+			retorno = nome + ", você não pode votar";
+			
+		} else if (idade >= 16 && idade <= 17 || idade > 70) {
+			retorno = nome + ", seu voto é facultativo";
+			
+		} else if (idade >= 18 && idade <= 70) {
+			retorno = nome + ", seu voto é obrigatório";
+		}
+		
+		return retorno;
+	}
+	
+	
+	/*
+	 * Retorna o ano atual (do seu computador)
+	 */
+	private static int retornaAnoAtual() {
+		return Calendar.getInstance().get(Calendar.YEAR);
+	}
+}
