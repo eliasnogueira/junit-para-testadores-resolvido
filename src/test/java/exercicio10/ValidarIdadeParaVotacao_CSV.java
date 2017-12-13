dd3c954ea811f17d31b342a6c199b7090a1e97fd
eliasnogueira <eliasnogueira@192.168.0.3>
2017-12-13 20:14:16 -0200

    Exercicio 10 adicionado

---
+++ src/test/java/exercicio10/ValidarIdadeParaVotacao_CSV.java
@@ -0,0 +1,21 @@
+package exercicio10;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+import junitparams.mappers.CsvWithHeaderMapper;
+import votacao.Votacao;
+
+@RunWith(JUnitParamsRunner.class)
+public class ValidarIdadeParaVotacao_CSV {
+	
+	@Test
+	@FileParameters(value = "src/test/resources/massa_de_dados.csv", mapper = CsvWithHeaderMapper.class)
+	public void validaObrigatoriedadeVoto(String nome, int anoDeNascimento, String resultado) {
+		assertEquals(resultado, Votacao.podeVotar(nome, anoDeNascimento));
+	}
+}
